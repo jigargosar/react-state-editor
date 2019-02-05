4760c8f1a8c1f103ebb73088daccd06f9104751a
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-05 20:37:19 +0530

    init

--- src/LogMessages.js
+++ src/LogMessages.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 export const LogMessages = observer(({ logStore }) => {
   return (
-    <div className="mv3">
-      <div className="b ttu tracked pv1">Logs</div>
+    <div className="mv3 f7">
+      <div className="pv1 b ttu tracked">Logs</div>
       {logStore.messages.map((msg, idx) => (
         <div key={idx} className="pv1 ph2">
           {msg}
