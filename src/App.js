4f3565fb5f5efb29c0882fb99cd8e0fe8f250bb5
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-05 20:37:53 +0530

    init

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = observer(() => {
     <div className="w-80 center">
       <div className="mv3 f4 ttu tracked">State Editor</div>
       <div className="mv3">State Tree</div>
-      <StateTree state={state} />
+      <StateTree state={state} logStore={logStore} />
       <LogMessages logStore={logStore} />
     </div>
   )
