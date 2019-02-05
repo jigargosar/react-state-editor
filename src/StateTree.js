b949a4fab121c94d7cb5dcbfbb4be2aa46acefd6
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-05 19:56:15 +0530

    init

--- src/StateTree.js
+++ src/StateTree.js
@@ -3,7 +3,7 @@ import { observer } from 'mobx-react-lite'
 import * as R from 'ramda'
 
 const StateTree = observer(({ state }) => {
-  return <div className="">{R.keys(state)}</div>
+  return <div className="">Root Keys : [{R.keys(state)}]</div>
 })
 
 StateTree.displayName = 'StateTree'
