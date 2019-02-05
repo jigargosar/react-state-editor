bf918bc7cd5cfd41f93db68b2d2b8b92936edb3b
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-05 19:27:35 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { observer } from 'mobx-react-lite'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = observer(() => {
+  return (
+    <div className="w-80 center">
+      <div className="mv3 f4 ttu tracked">State Editor</div>
+      <div className="mv3">State Tree</div>
+    </div>
+  )
+})
 
-export default App;
+App.displayName = 'App'
+
+export default App
