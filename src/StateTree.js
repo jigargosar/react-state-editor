e5d3456b27860b2203614cd0a16667aa95e90f08
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-05 20:52:00 +0530

    init

--- src/StateTree.js
+++ src/StateTree.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { observer } from 'mobx-react-lite'
 import * as R from 'ramda'
+
+const KeyValue = observer(({ state }) => {
+  return (
+    <>
+      {R.pipe(
+        R.mapObjIndexed(renderKV),
+        R.values,
+      )(state)}
+    </>
+  )
+
+  function renderKV(v, k) {
+    return (
+      <Fragment key={k}>
+        <div className="dt-row">
+          <div className="dtc pa1 ">{`${k}`}</div>
+          <div className="dtc pa1">{`${v}`}</div>
+        </div>
+      </Fragment>
+    )
+  }
+})
+
+KeyValue.displayName = 'KeyValue'
 
 const StateTree = observer(({ state }) => {
   return (
     <>
+      <KeyValue state={state} />
       <div className="">Root Keys : [{R.keys(state).join(', ')}]</div>
       <div className="">Root Values : [{R.values(state).join(', ')}]</div>
     </>
