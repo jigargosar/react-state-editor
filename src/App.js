2e63730f4b1dc39ae9299d40a55e7d0d01f17927
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-05 20:33:35 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { observer } from 'mobx-react-lite'
-import { observable } from 'mobx'
+import { extendObservable, observable } from 'mobx'
 import StateTree from './StateTree'
 import { LogMessages } from './LogMessages'
 
 const state = observable.object(
   {
-    appName: 'State Editor',
+    appName: 'Title: State Editor',
     a: 1,
     b: false,
   },
@@ -14,17 +14,23 @@ const state = observable.object(
   { name: 'RootState' },
 )
 
-const App = observer(() => {
-  const messages = observable.array(['log1 ', 'log2'], {
-    name: 'Log Messages',
-  })
+const logStore = (() => {
+  const store = observable.object(
+    { messages: observable.array([]) },
+    null,
+    { name: 'LogStore' },
+  )
 
+  return extendObservable(store, {})
+})()
+
+const App = observer(() => {
   return (
     <div className="w-80 center">
       <div className="mv3 f4 ttu tracked">State Editor</div>
       <div className="mv3">State Tree</div>
       <StateTree state={state} />
-      <LogMessages messages={messages} />
+      <LogMessages logStore={logStore} />
     </div>
   )
 })
--- src/LogMessages.js
+++ src/LogMessages.js
@@ -1,11 +1,11 @@
 import { observer } from 'mobx-react-lite'
 import React from 'react'
 
-export const LogMessages = observer(({ messages }) => {
+export const LogMessages = observer(({ logStore }) => {
   return (
     <div className="mv3">
       <div className="ttu tracked pv1">Logs</div>
-      {messages.map((msg, idx) => (
+      {logStore.messages.map((msg, idx) => (
         <div key={idx} className="pv1 ph2">
           {msg}
         </div>
